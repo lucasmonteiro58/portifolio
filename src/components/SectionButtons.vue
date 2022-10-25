ad998f0bd19e06cd4d361f5e201fa4634968b701
Lucas Monteiro <lucas.monteiro58@gmail.com>
2022-10-25 08:50:09 -0300

    add section buttons

---
+++ src/components/SectionButtons.vue
@@ -0,0 +1,35 @@
+<script setup>
+const portifolio = ref(true);
+function tooglePortifolio() {
+  portifolio.value = !portifolio.value;
+}
+</script>
+
+<template>
+  <div
+    class="flex justify-center mt-10 flex-wrap bg-white px-4 py-4 rounded-xl w-fit dark:bg-[#171f26]"
+  >
+    <button
+      @click="tooglePortifolio"
+      class="px-8 py-4 rounded-md font-medium min-w-[240px]"
+      :class="
+        portifolio
+          ? 'bg-[#d7d7d7] dark:bg-[#0c151d]'
+          : 'bg-white dark:bg-[#171f26]'
+      "
+    >
+      {{ $t("portifolio") }}
+    </button>
+    <button
+      @click="tooglePortifolio"
+      class="= px-8 py-4 rounded-md font-medium min-w-[240px]"
+      :class="
+        portifolio
+          ? 'bg-white dark:bg-[#171f26]'
+          : 'bg-[#d7d7d7] dark:bg-[#0c151d]'
+      "
+    >
+      {{ $t("aboutMe") }}
+    </button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <script setup>
 import ActionButtons from "../components/ActionButtons.vue";
 import ProfilePhoto from "../components/ProfilePhoto.vue";
+import SectionButtons from "../components/SectionButtons.vue";
 </script>
 
 <template>
   <div class="w-full sm:w-[700px]">
-    <div>
+    <div class="flex flex-col items-center">
       <ProfilePhoto></ProfilePhoto>
       <ActionButtons></ActionButtons>
+      <SectionButtons></SectionButtons>
     </div>
   </div>
 </template>
