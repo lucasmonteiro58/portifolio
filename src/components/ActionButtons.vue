de029c194d72cb49f901276c541a77a7b091f0d8
LUCAS MONTEIRO RIBEIRO <lucas.monteiro58@gmail.com>
2022-12-07 11:17:27 -0300

    add curriculo e contato link

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -2,16 +2,25 @@
 
 <template>
   <div class="flex justify-center mt-10 sm:gap-x-8 gap-x-4 gap-y-4">
-    <button
-      class="bg-blueLight sm:px-8 px-5 py-4 rounded-md hover:bg-[#92bafb] dark:text-grayText font-medium shadow-sm active:shadow-md sm:min-w-[200px]"
+    <a
+      download="CV - Lucas Monteiro Ribeiro.pdf"
+      href="https://drive.google.com/file/d/1lSb6KVK04qngrB1K1C5olXfboPTLCPjR/view?usp=sharing"
+      target="_blank"
     >
-      {{ $t("downloadCV") }}
-      <IconifyIcon icon="charm:download" class="ml-1 inline"></IconifyIcon>
-    </button>
-    <button
-      class="bg-white sm:px-12 px-5 py-4 rounded-md font-medium dark:bg-[#171f26] shadow-sm active:shadow-md sm:min-w-[200px]"
-    >
-      {{ $t("contactMe") }}
-    </button>
+      <button
+        class="bg-blueLight sm:px-8 px-5 py-4 rounded-md hover:bg-[#92bafb] dark:text-grayText font-medium shadow-sm active:shadow-md sm:min-w-[200px]"
+      >
+        {{ $t("downloadCV") }}
+        <IconifyIcon icon="charm:download" class="ml-1 inline"></IconifyIcon>
+      </button>
+    </a>
+
+    <a href="https://wa.me/+5585998027993" target="_blank">
+      <button
+        class="bg-white sm:px-12 px-5 py-4 rounded-md font-medium dark:bg-[#171f26] shadow-sm active:shadow-md sm:min-w-[200px]"
+      >
+        {{ $t("contactMe") }}
+      </button>
+    </a>
   </div>
 </template>
