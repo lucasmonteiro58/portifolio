8bc7b070ace297060dee51608a5fed35ad2bd812
LUCAS MONTEIRO RIBEIRO <lucas.monteiro58@gmail.com>
2023-04-03 10:23:15 -0300

    add links

--- src/components/ProfilePhoto.vue
+++ src/components/ProfilePhoto.vue
@@ -9,8 +9,12 @@
         {{ $t("frontEnd-developer") }}
       </div>
       <div class="flex gap-x-2 text-grayText dark:text-grayLight mt-2">
-        <IconifyIcon icon="akar-icons:github-fill"></IconifyIcon>
-        <IconifyIcon icon="akar-icons:linkedin-box-fill"></IconifyIcon>
+        <a href="https://github.com/lucasmonteiro58" target="_blank"
+          ><IconifyIcon icon="akar-icons:github-fill"></IconifyIcon
+        ></a>
+        <a href="https://www.linkedin.com/in/llucasmonteiro/" target="_blank"
+          ><IconifyIcon icon="akar-icons:linkedin-box-fill"></IconifyIcon
+        ></a>
       </div>
     </div>
   </div>
