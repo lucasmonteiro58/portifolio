734b3997b379a08aac10e7ff2d86dd822c5f220a
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-04-30 13:08:55 -0300

    Refactor code structure for improved readability and maintainability

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 // Importe outros serviços do Firebase que você precisar (auth, storage, etc)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2paIeD9p22f0D9CaI5Mo-Sex5PanBZF4",
-  authDomain: "portifolio-ugspbc.firebaseapp.com",
-  projectId: "portifolio-ugspbc",
-  storageBucket: "portifolio-ugspbc.firebasestorage.app",
-  messagingSenderId: "910631880568",
-  appId: "1:910631880568:web:10a7cd4a7011772868aa8d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 // Inicialize o Firebase
 const firebaseApp = initializeApp(firebaseConfig);
